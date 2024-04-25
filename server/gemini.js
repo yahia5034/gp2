d7f6506905cdbc45fae20fabecd966878a1f790a
yahiaMohammed <yahia.mohammed.mahmoud@gmail.com>
2024-04-25 19:42:10 +0200

    rm require.env

--- server/gemini.js
+++ server/gemini.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
