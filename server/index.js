1807bb1d0ffefdc951c540cbceeb370a2f1c7387
yahiaMohammed <yahia.mohammed.mahmoud@gmail.com>
2024-04-27 15:31:20 +0200

    update connection and fixing the error

--- server/index.js
+++ server/index.js
@@ -20,10 +20,10 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/control', async (req, res) => {
-    const msg= req.body.message;
-    switch(msg){
+    const message= req.body.message;
+    console.log(message);
+    switch(message){
         case "describe":
-        case "map":
         case "lidar":
         case "find": 
     /*New Area*/
@@ -74,18 +74,29 @@ app.post('/api/control', async (req, res) => {
             req2.end();
             break;
   /*End of new area*/
-    default:
-        try {
-            console.log(req.body);
-            const message = req.body.message;
-            const result = await run(message);
-            console.log('Received message:', message);
-            console.log(result);
-            res.json({ message: result });
-        } catch (error) {
-            console.error("Error occurred in API control endpoint:", error);
-            res.status(500).json({ error: "Internal server error" });
-        }
+        /* These cases are handled in the Angular Typescript....
+        case "maps":
+            //maps code call abdo .....
+            break;
+        case "go":
+            // call abdo ....
+            break;
+        case "read":
+            //call abdo ocr
+            break;
+        */
+        default:
+            try {
+                console.log(req.body);
+                const message = req.body.message;
+                const result = await run(message);
+                console.log('Received message:', message);
+                console.log(result);
+                res.json({ message: result });
+            } catch (error) {
+                console.error("Error occurred in API control endpoint:", error);
+                res.status(500).json({ error: "Internal server error" });
+            }
     }
 });
 
@@ -145,4 +156,4 @@ app.get('/',(req,res)=>{
 })
 app.listen(process.env.PORT || 3002, function () {
     console.log("connected");
-  });
+});
