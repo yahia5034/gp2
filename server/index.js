d3f3be8648f1e0cd1eaf339acfc57334b0620504
yahiaMohammed <yahia.mohammed.mahmoud@gmail.com>
2024-04-27 19:59:25 +0200

    OCR FIXED

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const multer = require('multer');
 const { spawn } = require('child_process');
 const https = require('https')
+const Tesseract =require("tesseract.js");
 
 const app = express();
 // const corsOptions = {
@@ -102,56 +103,35 @@ app.post('/api/control', async (req, res) => {
 });
 
 // Multer configuration for handling file uploads
-const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./uploads/');
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
     }
-});
+})
+const upload= multer({storage:storage});
 
-const upload = multer({ storage: storage });
+app.post('/api/upload',upload.single('uploadedImage'),(req,res)=>{
+    console.log(req.file);
 
-// POST endpoint to receive image uploads
-app.post('/upload', upload.single('image'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded' });
+    try{
+        Tesseract.recognize(
+            'uploads/'+req.file.filename,
+            'eng',
+            {logger:m =>console.log(m)}
+        ).then(({data:{text}})=>{
+            return res.json(
+                {
+                    message:text
+                }
+            )
+        })          
+    }catch(error){
+        console.error(error)
     }
-
-    // Spawn a child process to run the Python script
-    const pythonProcess = spawn('python', ['./python.py', req.file.path]);
-
-    let result = '';
-
-    pythonProcess.stdout.on('data', (data) => {
-        result += data.toString();
-    });
-
-    pythonProcess.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });
-
-    pythonProcess.stdout.on('end', () => {
-        console.log('Data from Python script received completely');
-        console.log(`Result from Python script: ${result}`);
-        
-        // Parse the JSON result
-        try {
-            const parsedResult = JSON.parse(result);
-            console.log(`Parsed result: ${parsedResult.text}`);
-            
-            // Send the extracted text to the client
-            res.json({ text: parsedResult.text });
-            
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    });
-
-    pythonProcess.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });
-});
+ })
 app.get('/',(req,res)=>{
     res.send("<h1>HElloo to server<h1/>")
 })
